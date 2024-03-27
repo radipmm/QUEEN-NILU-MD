7e23b242cddbac1b5165c8649deaa21d5f11f158
radipmm <165175599+radipmm@users.noreply.github.com>
2024-03-27 20:49:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ function convertToBool(text, fault = 'true') {
 
 // PUT YOUR SESSION ID HERE  ##Required 
 
-global.SESSION_ID = '' // PUT your session ID BY scan QR 
+global.SESSION_ID = 'QUEENNILU;;;xut1mZJK#QdYmbYjewk6e8eS6SFpx9Se3eAvDsg7jvafDX04rdio' // PUT your session ID BY scan QR 
 
 // databse URL  ##Required 
 global.POSTGRESQL_URL = ''
